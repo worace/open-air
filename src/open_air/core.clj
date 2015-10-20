5a23c1c28b81258cc2c8bf47e40f1925591ca711
worace <horace.d.williams@gmail.com>
2015-10-20 07:00:39 -0600

    starting on TAT open air project¡

---
+++ src/open_air/core.clj
@@ -0,0 +1,12 @@
+(ns open-air.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+(def pizza "hi")
+
+
+(defn some-fn [arg] (println arg))
---
+++ test/open_air/core_test.clj
@@ -0,0 +1,7 @@
+(ns open-air.core-test
+  (:require [clojure.test :refer :all]
+            [open-air.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
