a621018358a3efcbccffdf8c1a1f41bba499955e
Horace Williams <horace.d.williams@gmail.com>
2015-11-17 08:07:28 -0700

    declare main namespace

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main open-air.core
   :dependencies [
                   [org.clojure/clojure "1.6.0"]
                   [clj-http "2.0.0"]
